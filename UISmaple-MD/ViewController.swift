5bacdd104ef024dbaebbf49f64f5acc9f29d67a1
markfour <axhfaro@gmail.com>
2017-06-07 00:04:02 +0900

    Initilize

---
+++ UISmaple-MD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UISmaple-MD
+//
+//  Created by kazumi hayashida on 2017/06/07.
+//  Copyright © 2017年 organization name. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
